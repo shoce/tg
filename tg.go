b561157e0c67d66793a018746d391c501e70e674
shoce <yogze@icloud.com>
2025-04-24 01:38:02 +0530

    IST

--- tg.go
+++ tg.go
@@ -874,7 +874,7 @@ func safestring(s string) (t string) {
 }
 
 func ts() string {
-	tnow := time.Now().UTC()
+	tnow := time.Now().In(time.FixedZone("IST", 330*60))
 	return fmt.Sprintf(
 		"%d:%02d%02d:%02d%02d+",
 		tnow.Year()%1000, tnow.Month(), tnow.Day(),
